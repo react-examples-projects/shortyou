1d925a974c0ab61ea30f5b08757abce93e05bd16
Libardo <libardojesusrengifo129@gmail.com>
2023-03-06 20:53:02 +0100

    api host changed

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
+const API_HOST = "https://shortyou-api.vercel.app/api/";
+
 export const fetchPosts = async () => {
-  const res = await axios.get("http://localhost:5000/api/post");
+  const res = await axios.get(API_HOST + "post");
   const posts = res.data?.data;
   return posts;
 };
 
 export const createPost = async (fd) => {
-  const res = await axios.post("http://localhost:5000/api/post", fd);
+  const res = await axios.post(API_HOST + "post", fd);
   const post = res.data?.data;
   return post;
 };
 
 export const searchPosts = async (search) => {
-  const res = await axios.get(
-    "http://localhost:5000/api/search?query=" + search
-  );
+  const res = await axios.get(API_HOST + "search?query=" + search);
   const posts = res.data?.data;
   return posts;
 };
